cd98ac892774c87bc1e999608e6de51a0696d4df
Rajnikanth1999 <praveendba31@gmail.com>
2022-11-04 16:19:32 +0530

    Added ansible install commands in provisioner

---
+++ provision.tf
@@ -0,0 +1,20 @@
+resource "null_resource" "apache" {
+  triggers = {
+    trigger = var.trigger
+  }
+  provisioner "remote-exec" {
+    inline = [
+        "sudo apt update",
+        "sudo apt-add-repository ppa:ansible/ansible",
+        "sudo apt install ansible -y"
+    ]
+    connection {
+    host = azurerm_public_ip.publicip.ip_address
+    password = var.web_pass
+    user = var.web_user
+    }
+  }
+  depends_on = [
+    azurerm_virtual_machine.app
+  ]
+}
